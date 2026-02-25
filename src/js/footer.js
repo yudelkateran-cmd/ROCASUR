73a52cb70c39e2f801283af0293e4b5d4fe2797c
Yudelka Reyes <yudelkateran@gmail.com>
2026-02-25 19:41:09 -0300

    Agregando Detalles

--- src/js/footer.js
+++ src/js/footer.js
@@ -7,12 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div class="footer-left">
                     <h3 class="footer-slogan">
                         <span class="slogan-es">Desde Chile para el Mundo: Minerales Esenciales, Futuro Sostenible /</span>
-                        <span class="slogan-en">From Chile to the World: Essential Minerals, Sustainable Future</span>
+                        <span class="slogan-en">From Chile to the World: Essential Minerals, Sustainable Future </span>
+                        <span class="copyright">@2026 ROCASUR</span>
                     </h3>
                 </div>
                 <div class="footer-right">
                     <svg class="footer-star" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8 0L13.5 10.5L24 12L13.5 13.5L12 24L10.5 13.5L0 12L10.5 10.5L12 0Z" fill="white" opacity="0.6"/>
+                       <path d="M8 0L13.5 10.5L24 12L13.5 13.5L12 24L10.5 13.5L0 12L10.5 10.5L12 0Z" fill="white" opacity="0.6"/>
                     </svg>
                 </div>
             </footer>
