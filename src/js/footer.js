a18a87e4b3f1f7ae43059823d657a20c400b423d
Yudelka Reyes <yudelkateran@gmail.com>
2026-02-24 22:08:31 -0300

    Agregando acerca

--- src/js/footer.js
+++ src/js/footer.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 </div>
                 <div class="footer-right">
                     <svg class="footer-star" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 0L13.5 10.5L24 12L13.5 13.5L12 24L10.5 13.5L0 12L10.5 10.5L12 0Z" fill="white" opacity="0.6"/>
+                        <path d="M8 0L13.5 10.5L24 12L13.5 13.5L12 24L10.5 13.5L0 12L10.5 10.5L12 0Z" fill="white" opacity="0.6"/>
                     </svg>
                 </div>
             </footer>
