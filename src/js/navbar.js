7c943a1675f998027ef7ce5308c10a3916c1f3b9
Yudelka Reyes <yudelkateran@gmail.com>
2026-02-24 22:45:41 -0300

    Agregando formulario

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -15,8 +15,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 
                 <nav class="nav-links">
                     <ul>
-                        <li><a href="#">Acerca de Nosotros <span>&#711;</span></a></li>
-                        <li><a href="#">Productos <span>&#711;</span></a></li>
+                        <li><a href="#acerca-de">Acerca de Nosotros<span>&#711;</span></a></li>
+                        
+                        <li class="dropdown">
+                            <a href="#productos">Productos <span>&#711;</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#cobre">Concentrado de Cobre</a></li>
+                                <li><a href="#oro">Concentrado de Oro</a></li>
+                            </ul>
+                        </li>
+                        
                     </ul>
                 </nav>
                 
